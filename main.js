fd73eea5c97f53c90ee3db4121ee190423c2ba8d
Simon Tulling <s.a.tulling@student.tudelft.nl>
2020-11-25 23:57:48 +0100

    Something resembling actual results.
    Javascript = :(

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var ingredients = []
 var nIngredients = 0
 var squareMatrix = []
 var connections = [];
+var items = [];
+var whitelist = [];
 
 function matrix(m, n) {
     var result = []
@@ -16,22 +18,71 @@ function main() {
 		function (data) {
 			ingredients.push(JSON.parse(data.ingredient_ids))
 	}).then( function () {
-		var tmp = ingredients.map(x => Math.max(...x))
-		for (let i = 0; i < tmp.length; i++) {
-		  if (tmp[i] > nIngredients) {
-			nIngredients = tmp[i];
-		  }
-		}
-		connections = matrix(nIngredients, nIngredients);
+		var counts = {};
 		for (let i = 0; i < ingredients.length; i++) {
-			if (ingredients[i].length > 1) {
-				for (var j = 0; j < ingredients[i].length - 1; j++) {
-					for (var k = j+1; k < ingredients[i].length; k++) {
-						connections[j][k] += 1;
+			for (var j = 0; j < ingredients[i].length - 1; j++) {
+				var num = ingredients[i][j]
+				counts[num] = counts[num] ? counts[num] + 1 : 1;
+			}
+		}
+		items = Object.keys(counts).map(function(key) {
+		  return [key, counts[key]];
+		});
+
+		// Sort the array based on the second element
+		items.sort(function(first, second) {
+		  return second[1] - first[1];
+		});
+		
+		items = items.slice(0, 20);
+		console.log(items)
+		whitelist = items.map(x => parseInt(x[0]))
+		connections = matrix(20, 20);
+		for (var recipe of ingredients) {
+			matches = recipe.filter(value => whitelist.includes(value))
+			if (matches.length > 1) {
+				for (var i = 0; i < matches.length - 1; i++) {
+					for (var j = i+1; j < matches.length; j++) {
+						connections[whitelist.indexOf(matches[i])][whitelist.indexOf(matches[j])] += 1;
 					}
 				}
 			}
 		}
+		
+		var res = d3.chord()
+		.padAngle(0.05)     // padding between entities (black arc)
+		.sortSubgroups(d3.descending)
+		(connections)
+		
+		// add the groups on the inner part of the circle
+		svg
+		  .datum(res)
+		  .append("g")
+		  .selectAll("g")
+		  .data(function(d) { return d.groups; })
+		  .enter()
+		  .append("g")
+		  .append("path")
+			.style("fill", "grey")
+			.style("stroke", "black")
+			.attr("d", d3.arc()
+			  .innerRadius(200)
+			  .outerRadius(210)
+			)
+
+		// Add the links between groups
+		svg
+		  .datum(res)
+		  .append("g")
+		  .selectAll("path")
+		  .data(function(d) { return d; })
+		  .enter()
+		  .append("path")
+			.attr("d", d3.ribbon()
+			  .radius(200)
+			)
+			.style("fill", "#69b3a2")
+			.style("stroke", "black");
 	})
 	
 	var svg = d3.select("#visualization")
@@ -40,42 +91,6 @@ function main() {
 		.attr("height", 800)
 	  .append("g")
 		.attr("transform", "translate(400,400)")
-
-
-	var res = d3.chord()
-		.padAngle(0.05)     // padding between entities (black arc)
-		.sortSubgroups(d3.descending)
-		(connections)
-		
-	// add the groups on the inner part of the circle
-	svg
-	  .datum(res)
-	  .append("g")
-	  .selectAll("g")
-	  .data(function(d) { return d.groups; })
-	  .enter()
-	  .append("g")
-	  .append("path")
-		.style("fill", "grey")
-		.style("stroke", "black")
-		.attr("d", d3.arc()
-		  .innerRadius(200)
-		  .outerRadius(210)
-		)
-
-	// Add the links between groups
-	svg
-	  .datum(res)
-	  .append("g")
-	  .selectAll("path")
-	  .data(function(d) { return d; })
-	  .enter()
-	  .append("path")
-		.attr("d", d3.ribbon()
-		  .radius(200)
-		)
-		.style("fill", "#69b3a2")
-		.style("stroke", "black");
 
 	/*
 	d3.csv("./data/PP_recipes.csv", function(data) {
