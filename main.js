217f105480e7c9e93ba53c18f01aca9beebf32cb
Simon Tulling <simon.tulling99@gmail.com>
2020-11-25 23:00:44 +0100

    Load kaggle dataset

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
+var ingredients = []
+var nIngredients = 0
+var squareMatrix = []
+var connections = [];
+
+function matrix(m, n) {
+    var result = []
+    for(var i = 0; i < n; i++) {
+        result.push(new Array(m).fill(0))
+    }
+    return result
+}
+
 function main() {
+	d3.csv("./data/PP_recipes.csv", 
+		function (data) {
+			ingredients.push(JSON.parse(data.ingredient_ids))
+	}).then( function () {
+		var tmp = ingredients.map(x => Math.max(...x))
+		for (let i = 0; i < tmp.length; i++) {
+		  if (tmp[i] > nIngredients) {
+			nIngredients = tmp[i];
+		  }
+		}
+		connections = matrix(nIngredients, nIngredients);
+		for (let i = 0; i < ingredients.length; i++) {
+			if (ingredients[i].length > 1) {
+				for (var j = 0; j < ingredients[i].length - 1; j++) {
+					for (var k = j+1; k < ingredients[i].length; k++) {
+						connections[j][k] += 1;
+					}
+				}
+			}
+		}
+	})
+	
 	var svg = d3.select("#visualization")
 	  .append("svg")
-		.attr("width", 440)
-		.attr("height", 440)
+		.attr("width", 800)
+		.attr("height", 800)
 	  .append("g")
-		.attr("transform", "translate(220,220)")
+		.attr("transform", "translate(400,400)")
 
-	var matrix = [
-	  [11975,  5871, 8916, 2868],
-	  [ 1951, 10048, 2060, 6171],
-	  [ 8010, 16145, 8090, 8045],
-	  [ 1013,   990,  940, 6907]
-	];
 
 	var res = d3.chord()
 		.padAngle(0.05)     // padding between entities (black arc)
 		.sortSubgroups(d3.descending)
-		(matrix)
+		(connections)
 		
 	// add the groups on the inner part of the circle
 	svg
